bed28705bfdd745794e154807b8857a619cfd15b
GitHub Action <action@github.com>
2025-03-23 16:43:43 +0000

    Update formula to v0.1.5

--- asimeow.rb
+++ asimeow.rb
@@ -7,7 +7,6 @@ class Asimeow < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mdnmdn/asimeow/releases/download/v0.1.5/asimeow-aarch64-apple-darwin.zip"
-           https://github.com/mdnmdn/asimeow/releases/download/v0.1.4/asimeow-aarch64-apple-darwin.zip
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
       url "https://github.com/mdnmdn/asimeow/releases/download/v0.1.5/asimeow-x86_64-apple-darwin.zip"

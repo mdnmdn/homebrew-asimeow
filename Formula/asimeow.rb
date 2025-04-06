6da5398cd470576f4390c2c4e6e2013cd1e2b9c0
GitHub Action <action@github.com>
2025-04-06 18:01:30 +0000

    Update formula to v0.2.0

--- Formula/asimeow.rb
+++ Formula/asimeow.rb
@@ -1,16 +1,16 @@
 class Asimeow < Formula
   desc "A tool for managing macOS Time Machine exclusions for developer projects"
   homepage "https://github.com/mdnmdn/asimeow"
-  version "0.1.9"
+  version "0.2.0"
   license "MIT"  # Replace with your actual license
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mdnmdn/asimeow/releases/download/v0.1.9/asimeow-aarch64-apple-darwin.zip"
-      sha256 "7b8e923e4851637b72ff09d2204204e1ff30f1c3b27443a4f2d6e801b71bbcea"
+      url "https://github.com/mdnmdn/asimeow/releases/download/v0.2.0/asimeow-aarch64-apple-darwin.zip"
+      sha256 "a5ea2a8fd53f099ee984dee5f53e14c6f046811a3950d3fade0485607c0fa5d4"
     else
-      url "https://github.com/mdnmdn/asimeow/releases/download/v0.1.9/asimeow-x86_64-apple-darwin.zip"
-      sha256 "439126aa5956042d1f5971f4254a61882976f648021e6f2d27723dc065cb7ae0"
+      url "https://github.com/mdnmdn/asimeow/releases/download/v0.2.0/asimeow-x86_64-apple-darwin.zip"
+      sha256 "21273a826bcc6c1bb09e14754d529cb387fecd2f54d882211750a64f37ada8ae"
     end
   end
 
